030383f73b512b21fccbf41dc092b9ba6690fb32
007lazi@gmail.com <007lazi@gmail.com>
2025-01-07 22:08:23 +0100

    live

--- environment.ts
+++ environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     base_url:'https://www.gobusly.com',
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "pk_test_51K1DdaDAZApOs2EVXCiMmQnlAa9TIqCpnuhrDrpiKqdTGuGlNvbbyYnaEPgl2m0Qg2WfBC6r6j2wfP2jLdDwPdnm00D2bcqz6v",
     STRIPE_SECRET_KEY: "sk_test_51K1DdaDAZApOs2EV0Ig7LD6s4lAqAHm3tk9j3SKl5yhKqbDkm8xsUXgpJZ0PL8s0eXm7MK7dBM7ojhHmn0f3BosX00ICXrXpbV",
-    GOOGLE_WEB_CLIENT_ID:"387457533430-ut8n5hgjki836vm38nonll9ve7mtdaif.apps.googleusercontent.com",
-    GOOGLE_WEB_CLIENT_SECRET:"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",
-    GOOGLE_IOS_CLIENT_ID:"387457533430-hp0pbc8b4ibs64lgvveev1bcvda8eqds.apps.googleusercontent.com",
+    // GOOGLE_WEB_CLIENT_ID:"387457533430-ut8n5hgjki836vm38nonll9ve7mtdaif.apps.googleusercontent.com",
+    // GOOGLE_WEB_CLIENT_SECRET:"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",
+    // GOOGLE_IOS_CLIENT_ID:"387457533430-hp0pbc8b4ibs64lgvveev1bcvda8eqds.apps.googleusercontent.com",
 }
